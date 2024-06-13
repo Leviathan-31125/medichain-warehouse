b4a6f0528fbc422e529bebdfc57d892d790034ac
Leviathan-31125 <athaidar.dev@gmail.com>
2024-06-13 23:02:29 +0700

    DO Mastr

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -10,7 +10,8 @@ class SalesOrderController extends Controller
     public function getAllSOMST () {
         $data = SOMST::with(['sodtl', 'customer', 'sodtl.stock'])
         ->where('fc_status', '!=', 'FINISH')
-        ->where('fc_status', '!=', 'LOCK')->get();
+        ->where('fc_status', '!=', 'LOCK')
+        ->where('fc_status', '!=', 'REQUEST')->get();
         return response()->json(['status' => 200, 'data' => $data]);
     }
 }
